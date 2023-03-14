a991c07083a392be411dd348fef9cf1ea3f1f36b
Ekram-ul-haque <ekram0096@gmail.com>
2023-03-14 16:43:55 +0530

    Major Banner changes and profile update

--- Js/script.js
+++ Js/script.js
@@ -2,8 +2,10 @@ burger = document.querySelector('.burger');
 navbar = document.querySelector('.navbar');
 navList = document.querySelector('.nav-list');
 logo = document.querySelector('.logo');
+front = document.querySelector('.front');
 
 burger.addEventListener('click', ()=> {
+    front.classList.toggle('expand-navbar');
     navbar.classList.toggle('small-nav');
     navList.classList.toggle('icons');
     logo.classList.toggle('icons');
