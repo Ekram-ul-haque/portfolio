8e7a28cb3d10e681405ab0ffd0b5ee2e69fdb442
Ekram-ul-haque <ekram0096@gmail.com>
2023-03-18 18:33:03 +0530

    experience added

--- Js/script.js
+++ Js/script.js
@@ -4,10 +4,29 @@ navList = document.querySelector('.nav-list');
 logo = document.querySelector('.logo');
 front = document.querySelector('.front');
 
-burger.addEventListener('click', ()=> {
+burger.addEventListener('click', () => {
     front.classList.toggle('expand-navbar');
     navbar.classList.toggle('small-nav');
     navList.classList.toggle('icons');
     logo.classList.toggle('icons');
     
 })
+
+const content_boxes = document.querySelectorAll('.content');
+
+window.addEventListener('scroll', contentBoxes);
+
+function contentBoxes() {
+    const triggerBottom = window.innerHeight;
+    
+    content_boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if (boxTop < triggerBottom) {
+            box.classList.add('show');
+        } else {
+            box.classList.remove('show');
+        }
+    })
+
+}
